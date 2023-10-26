0a6accc398ee6982009a36309808f4811182f9fa
joeyksclark <joeyksclark@gmail.com>
2023-10-26 09:01:57 -0700

    Add minimal home page

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+const HomePage: React.FC = () => {
+    const [productCount, setProductCount] = useState<number>(0);
+    const [orderCount, setOrderCount] = useState<number>(0);
+
+    useEffect(() => {
+        fetch('/api/products')
+            .then((response) => response.json())
+            .then((data) => {
+                setProductCount(data.length);
+            });
+
+        fetch('/api/orders')
+            .then((response) => response.json())
+            .then((data) => {
+                setOrderCount(data.length);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Welcome to the Reebelo Dashboard</h1>
+
+            <div>
+                <h2>Product Statistics</h2>
+                <p>Total Products: {productCount}</p>
+            </div>
+
+            <div>
+                <h2>Order Statistics</h2>
+                <p>Total Orders: {orderCount}</p>
+            </div>
+
+            <div>
+                <Link href="/products">View Products</Link>
+            </div>
+
+            <div>
+                <Link href="/orders">View Orders</Link>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
