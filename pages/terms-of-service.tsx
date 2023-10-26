304b9fe7c1d48a6f5548379659fcaa52a7e51dd6
joeyksclark <joeyksclark@gmail.com>
2023-10-26 13:33:24 -0700

    Styled the terms of service

--- pages/terms-of-service.tsx
+++ pages/terms-of-service.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const TermsOfServicePage: React.FC = () => {
     return (
-        <div>
-            <p>
-                No terms of service.
+        <div className="container mx-auto py-10">
+            <h1 className="text-4xl font-bold">Terms of Service</h1>
+
+            <p className="mt-5">
+                No terms. Enjoy the service.
             </p>
         </div>
     );
